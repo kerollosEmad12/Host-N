a55134626733e922c18676dcae6df4a4ba8cac53
Kerolos <kyrillosemad06@gmail.com>
2023-10-01 00:14:52 +0300

    Edit

--- src/test/java/TestCases/TestBase.java
+++ src/test/java/TestCases/TestBase.java
@@ -2,8 +2,6 @@ package TestCases;
 
 import Pages.P01HomePage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
